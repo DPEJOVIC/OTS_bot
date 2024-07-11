189ee19c53027fc70ca608b483494b5c36ff35ae
DPEJOVIC <102003169+DPEJOVIC@users.noreply.github.com>
2024-07-12 00:08:41 +1000

    Added scenarios and chat cutoff

---
+++ Home.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import json
+
+
+st.title("Operation Training Simulator Chatbot")
+
+
+def clearhistory(): 
+    if "chat_history" not in st.session_state:
+        st.session_state["chat_history"] = []
+    st.session_state["chat_history"] = []
+
+
+# Allow the user to select a scenario.
+scenario_selection = st.selectbox(
+    "Please select the scenario you completed below.",
+    ("Scenario 1", "Scenario 2", "Scenario 3", "Scenario 4", 
+     "Research Scenario 1", "Research Scenario 2", "Research Scenario 3", "Research Scenario 4", 
+     "Research Scenario 5", "Research Scenario 6", "Research Scenario 7"),
+    index = None,
+    on_change = clearhistory)
+
+
+# Sets the system prompt based on the user's selection
+def setprompt(context, s, cutoff):
+    if "system_prompt" not in st.session_state:
+        st.session_state["system_prompt"] = ""
+    
+    if "cutoff" not in st.session_state:
+        st.session_state["cutoff"] = ""
+
+    with open("scenarios.json", "r") as file:
+        data = json.load(file)
+        st.session_state["system_prompt"] = data[context] + "\n" + data[s]
+        st.session_state["cutoff"] = data[cutoff]
+    
+
+# If statement for user selection
+if scenario_selection == "Scenario 1":
+    setprompt("context", "s1", "s1cutoff")
+elif scenario_selection == "Scenario 2":
+    setprompt("context", "s2", "s2cutoff")
+elif scenario_selection == "Scenario 3":
+    setprompt("context", "s3", "s3cutoff")
+elif scenario_selection == "Scenario 4":
+    setprompt("context", "s4", "s4cutoff")
+elif scenario_selection == "Research Scenario 1":
+    setprompt("rscontext", "rs1", "rs1cutoff")
+elif scenario_selection == "Research Scenario 2":
+    setprompt("rscontext", "rs2", "rs2cutoff")
+elif scenario_selection == "Research Scenario 3":
+    setprompt("rscontext", "rs3", "rs3cutoff")
+elif scenario_selection == "Research Scenario 4":
+    setprompt("rscontext", "rs4", "rs4cutoff")
+elif scenario_selection == "Research Scenario 5":
+    setprompt("rscontext", "rs5", "rs5cutoff")
+elif scenario_selection == "Research Scenario 6":
+    setprompt("rscontext", "rs6", "rs6cutoff")
+elif scenario_selection == "Research Scenario 7":
+    setprompt("rscontext", "rs7", "rs7cutoff")
+
+
+# Prompt user to proceed to scenario
+if scenario_selection:
+    st.write("You've selected:", scenario_selection)
+    st.write("Please proceed to the chat window. ")
---
+++ pages/1_Scenario_Chat.py
@@ -0,0 +1,91 @@
+import streamlit as st
+from openai import OpenAI
+
+
+st.set_page_config(
+    page_title="Chat",
+    page_icon="👋",
+)
+
+
+st.title("Chat")
+
+
+# Set up  system prompt
+if "system_prompt" not in st.session_state:
+    st.session_state["system_prompt"] = ""
+
+
+# Set up OpenAI API client
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+
+# Select GPT model
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+
+# Add reload counter
+if "counter" not in st.session_state:
+    st.session_state.counter = 0
+st.session_state.counter += 1
+
+
+# Initialise chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+
+# Initialise response counter
+if "response_counter" not in st.session_state:
+    st.session_state.response_counter = 0
+
+
+# Write chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# Chat logic
+if prompt := st.chat_input("Ask the supervisor questions", disabled = st.session_state.response_counter >= 5):
+    
+    st.session_state.chat_history.append({"role": "user", "content": prompt})
+
+    if st.session_state.response_counter < 5:
+    
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            messages_with_system_prompt = [{"role": "system", "content": st.session_state["system_prompt"]}] + [
+                {"role": m["role"], "content": m["content"]}
+            for m in st.session_state.chat_history
+            ]
+
+            stream = client.chat.completions.create(
+                model = st.session_state["openai_model"],
+                messages = messages_with_system_prompt,
+                stream = True,
+            )
+            response = st.write_stream(stream)
+
+        st.session_state.response_counter += 1
+        st.session_state.chat_history.append({"role": "assistant", "content": response})
+    
+    else:
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            message_with_system_prompt = [{"role": "system", "content": f"Just repeat these words as closely as possible, but fix the formatting. You may add paragraph breaks where logical: {st.session_state["cutoff"]}"}]
+
+            stream = client.chat.completions.create(
+                    model = st.session_state["openai_model"],
+                    messages = message_with_system_prompt,
+                    stream = True,
+            )
+            response = st.write_stream(stream)
+            
+        st.session_state.chat_history.append({"role": "assistant", "content": response})
+
